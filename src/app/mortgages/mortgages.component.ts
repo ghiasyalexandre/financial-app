93a581ca86d4ad1533f51ee576999356015849c4
Ghiasy <ghiasyalexandre@gmail.com>
2025-01-03 17:43:21 -0500

    Banking Page/ Start Mortgage Page

--- src/app/mortgages/mortgages.component.ts
+++ src/app/mortgages/mortgages.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-mortgages',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mortgages.component.html',
   styleUrl: './mortgages.component.scss'
 })
 export class MortgagesComponent {
+  // Flag to control visibility of the alert bar
+  isAlertVisible = true;
 
+  // Method to hide the alert bar
+  hideAlert() {
+    this.isAlertVisible = false;
+  }
 }
