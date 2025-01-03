f388202cff696560ef47ecaeae1f0921fcd536b8
Ghiasy <ghiasyalexandre@gmail.com>
2025-01-03 15:52:19 -0500

    Banking Page Start

--- src/app/banking/banking.component.ts
+++ src/app/banking/banking.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-banking',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './banking.component.html',
   styleUrl: './banking.component.scss'
 })
 export class BankingComponent {
+  // Flag to control visibility of the alert bar
+  isAlertVisible = true;
 
+  // Method to hide the alert bar
+  hideAlert() {
+    this.isAlertVisible = false;
+  }
 }
