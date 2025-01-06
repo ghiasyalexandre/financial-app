674dc2739cc9d9c9c97205877eacd006994af0f5
Ghiasy <ghiasyalexandre@gmail.com>
2025-01-06 18:54:36 -0500

    Article Fix/Remove Home Equity

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import {MatTooltipModule} from '@angular/material/tooltip';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-home',
-  imports: [MatTooltipModule],
+  imports: [MatTooltipModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
